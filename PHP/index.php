5b8abf68d317b55b0fa4cd5df5158fe0be8eb7e0
Caroline <65663844+Caro-L-dev@users.noreply.github.com>
2022-08-24 17:28:52 +0200

    Add files via upload

--- PHP/index.php
+++ PHP/index.php
@@ -11,12 +11,16 @@ var_dump($firstnames);
 
 $firstnames[0] = "Cédric";
 var_dump($firstnames);
+
+
 echo "<br />";
 echo "<br />";
+echo "-----------------------------------";
 echo "<br />";
 
 
 // ===
+echo "<h2>EXO 1</h2>";
 
 // 1. Créer un tableau fruits contenant les valeurs : pomme, banane, orange, citron.
 $fruits = ["pomme", "banane", "orange", "citron"];
@@ -30,5 +34,50 @@ echo "<br />";
 
 // 3. Ajouter le fruit poire.
 $fruits[] = "poire";
-var_dump($fruits);
+
+// 4. Afficher le nouveau fruit
+echo $fruits[4];
+
+echo "<br />";
+echo "<br />";
+echo "-----------------------------------";
+echo "<br />";
+
+// ===
+echo "<h2>EXO 2</h2>";
+echo "<p>Les tableaux associatifs</p>";
+
+$person = [
+    'firstname' => 'John',
+    'name' => 'Dupont'
+];
+
+echo $person['firstname'];
+
+echo "<br />";
+echo "<br />";
+
+var_dump($person);
+
+$person['firstname'] ='François';
+var_dump($person);
+
+echo "<br />";
+echo "<br />";
+
+$person['age'] = 41;
+var_dump($person);
+    
+// EXO
+/*
+    Exercice 2
+    1. Créer un tableau associatif $car contenant :
+    - brand : Citroën
+    - model : C4
+    - year : 2018
+    - motor : Diesel
+    Afficher le tableau $car.
+    2. Modifier l'année pour mettre 2020
+    3. Ajouter la donnée power avec comme valeur : 120
+*/
 ?>
